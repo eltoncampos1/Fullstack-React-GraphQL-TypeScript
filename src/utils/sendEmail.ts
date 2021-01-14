03d43e38106fdadcfdf923f83d09db50322f806d
elnto de campos <eltoncampos36@gmail.com>
2021-01-14 19:28:06 -0300

    [Update] - send email forgot password

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,27 @@
+"use strict";
+import nodemailer from "nodemailer";
+
+export async function sendEmail(to: string, html: string) {
+
+  // const testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "mds43vi6nviwucqv@ethereal.email",
+      pass: "xJsQzVAuFYKqx5xUR9",
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>',
+    to,
+    subject: "Change password", 
+    html, 
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
