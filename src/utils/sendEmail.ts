4cfbc6008ba485c0bd276549785accb168e04c06
elnto de campos <eltoncampos36@gmail.com>
2021-01-15 17:48:57 -0300

    [Fix] - nodemailer error

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,33 +1,32 @@
 import nodemailer from "nodemailer";
-import smtpTransport from 'nodemailer-smtp-transport';
+// import smtpTransport from 'nodemailer-smtp-transport';
 
 
 export async function sendEmail(to: string, html: string) {
 
+  let testAccount = await nodemailer.createTestAccount();
 
-  let transporter = nodemailer.createTransport(smtpTransport({
-    service: 'gmail',
-    host:'smtp.gmail.com',
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
-      user: 'myemail@email.com', // generated ethereal user
-      pass: 'minhasenhasecreta', // generated ethereal password
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
     },
-  }));
+  });
 
-  var mailOptions = {
-    from: 'myemail@email.com',
-    to,
-    subject: 'Sending Email using Node.js[nodemailer]',
-    html,
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    }
-  });  
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
 }
